8c52c1fd2ed4963266577d8872e64e6f31d2c18f
t-kusakabe <ttmac0704@gmail.com>
2019-01-07 19:23:03 +0900

    fizz buzz

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,18 +1,19 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for FizzBuzz.
-  """
+  def run(limit) do
+    output(1, limit)
+  end
 
-  @doc """
-  Hello world.
+  defp output(index, limit) when index == limit do
+    cond do
+      rem(index, 15) == 0 -> IO.puts "FizzBuzz"
+      rem(index, 3)  == 0 -> IO.puts "Fizz"
+      rem(index, 5)  == 0 -> IO.puts "Buzz"
+      true                -> IO.puts index
+    end
+  end
 
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp output(index, limit) do
+    output(index, index)
+    output(index + 1, limit)
   end
 end
